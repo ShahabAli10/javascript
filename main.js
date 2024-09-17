942f6013e8e43474876c762c003df7004087fce9
Shahab Ali <shahabalisa08@gmail.com>
2024-09-17 15:30:21 +0500

    add

---
+++ main.js
@@ -0,0 +1,57 @@
+// alert("Welcome to javascript");
+// console.log(alert);
+
+// var a=5;
+// var b=10;
+// var c=a+b;
+// console.log(c);
+
+// var name1=10;
+// var city=26;
+// var merge=name1 + city;
+// console.log(merge);
+
+// let x=100;
+// x+15;
+// console.log(x+15);
+
+// var originalnum=30;
+// var newnum=originalnum + (-5);
+// console.log(newnum);
+
+// var originalnum=25;
+// var newnum=originalnum+2;
+// console.log(newnum);
+
+// var a=20
+// var b= a*a/a+a-a;
+// console.log(b);
+
+// var  message= 'par la';
+// alert(message);
+
+// var name="Shahab Ali";
+// var age="20";
+// var designation="Android developer";
+
+// function bio(){
+//     alert("Name:" + name);
+//     alert("Age:" + age);
+//     alert("Designation:" + designation);
+// }
+// bio()
+
+// let welcome = "welcome to js" ;
+// let happy = "Happy" ;
+// alert(welcome + "\n\n" + happy);
+
+// alert("you are in danger");
+// console.log("alert");
+
+// var input= prompt("Write Something you want:");
+// alert("Answer:" + input);
+
+// var input= prompt("your name:");
+// var input= prompt("age:");
+// alert ("Your name:" + input);
+// alert ("Your age:" + input);
