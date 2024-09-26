a404b5b7fa35d39d356106a80aba1c2bb4b83195
Shahab Ali <shahabalisa08@gmail.com>
2024-09-26 14:56:23 +0500

    add

---
+++ chapter 2/main.js
@@ -0,0 +1,21 @@
+var message="Hello world";
+alert(message);
+
+// 2. Write a scirpt to save students bio data in JS variable and show the data in alert boxes.
+var user=prompt("What is your name");
+alert(user);
+var age=prompt("Your age");
+alert(age);
+var work=prompt("what you do?");
+alert(work);
+
+// 3.Write a script to display the following alert using one JS variable:
+// alert("PIZZA" break "PIZZ" break "PIZ");
+
+// 4. Declare a variable called email and assign to it a string that represent your Email Address'
+var Email="My email address is: shahabalisa08@gmail.com";
+alert(Email);
+
+// 5. Declare a variable called book & give it the value.
+var book="I am trying to learn from the Book A smarter way to learn javascript";
+alert(book);
