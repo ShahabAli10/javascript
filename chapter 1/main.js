a7afc251a531bbc8df82b3d05c4c7c32e9a98a97
Shahab Ali <shahabalisa08@gmail.com>
2024-09-26 14:33:50 +0500

    add

---
+++ chapter 1/main.js
@@ -0,0 +1,12 @@
+// 1. write a script to display following message on your web page:
+alert("Error! Please enter a valid password");
+
+// 2. write a script to display following message on your web page:(Hint: Use Line break)
+alert("Welcome to js land...Happy Coding!");
+
+// 3. Write a script to display following messsage in sequence:
+alert("Welcome to JS Land...");
+alert("Happ Coding!");
+
+// 4.Generate the following message through browsers developer console:
+alert("Hello...I can run JS thruugh my web browser's console");
