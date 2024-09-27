1cd6ef7cdfd94eb7961d51e56efce5f46c7a8858
Shahab Ali <shahabalisa08@gmail.com>
2024-09-27 11:21:09 +0500

    add

---
+++ chapter 4/main.js
@@ -0,0 +1,8 @@
+var message = ("Variable names can only contain, $ and _ for example : $ my 1st variable");
+document.write(message);
+
+let var1 = 1, var2 = 2, var3 = 3;
+        
+console.log("var1:", var1);
+console.log("var2:", var2);
+console.log("var3:", var3);
